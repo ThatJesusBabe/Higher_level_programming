4f27ca7b5a5dff193cf776b195d4fb3b20bec4ce
Lorrine Adefemi <adefemilauren@gmail.com>
2022-12-19 14:19:10 -0800

    commit changes to task 0

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -2,10 +2,11 @@
 def safe_print_list(my_list=[], x=0):
     counter = 0
     for element in my_list:
-        try:
-            print(my_list[element], end='')
-            counter += 1
-        except IndexError:
-            break
+        if counter < x:
+            try:
+                print(my_list[element], end='')
+                counter += 1
+            except IndexError:
+                break
     print()
     return counter
