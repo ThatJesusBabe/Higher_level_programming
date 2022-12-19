c79670ca60e7681bb553f4108af38bd250f98242
Lorrine Adefemi <adefemilauren@gmail.com>
2022-12-19 14:01:05 -0800

    changes to task 4

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -8,9 +8,9 @@ def list_division(my_list_1, my_list_2, list_length):
         except(ValueError, TypeError):
             print("wrong type")
         except ZeroDivisionError:
-            print(division by 0)
+            print("division by 0")
         except IndexError:
-            print(out of range)
+            print("out of range")
         finally:
             new_list.append(quotient)
     return new_list
