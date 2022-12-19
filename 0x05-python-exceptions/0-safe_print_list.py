db7a09ef960c7c7dc176d3785ddc3e542c1f351c
Lorrine Adefemi <adefemilauren@gmail.com>
2022-12-19 13:25:46 -0800

    task 3

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -4,7 +4,7 @@ def safe_print_list(my_list=[], x=0):
 	for element in my_list:
 		try:
 			if counter < x:
-				print(f"{}".format(element), end = '\n')
+				print(my_list[element], end = '')
 				counter += 1
 		except IndexError:
 			break
