c617a353c8219d726093b550c9af4235244f1cf5
Lorrine Adefemi <adefemilauren@gmail.com>
2022-12-19 14:16:22 -0800

    commit changes to task 4

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 def list_division(my_list_1, my_list_2, list_length):
     new_list = []
-    for i in list_length:
+    for i in range(list_length):
         quotient = 0
         try:
             quotient = my_list_1[i] / my_list_2[i]
-        except(ValueError, TypeError):
+        except (ValueError, TypeError):
             print("wrong type")
         except ZeroDivisionError:
             print("division by 0")
