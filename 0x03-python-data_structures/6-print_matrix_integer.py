d04f995fe9a40f9b47e07624c5dd5965db68f829
Lorrine Adefemi <adefemilauren@gmail.com>
2023-03-07 07:00:06 -0800

    redoing taask

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -2,9 +2,9 @@
 
 def print_matrix_integer(matrix=[[]]):
     for row in matrix:
-        for colunm in row:
-             if colunm == row[-1]:
-                print("{}".format(colunm), end = "")
-             else:
-                print('{:d}'.format(colunm), end=' ')
-        print("")
+        for column in row:
+            if column == row[-1]:
+                print('{:d}'.format(column), end='')
+            else:
+                print('{:d}'.format(column), end=' ')
+        print()
