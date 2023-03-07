237ac044c0111601c4140879f79d4432ce9aa572
Lorrine Adefemi <adefemilauren@gmail.com>
2023-03-07 15:24:28 +0100

    Update 6-print_matrix_integer.py

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -4,5 +4,5 @@
 def print_matrix_integer(matrix=[[]]):
     for row in matrix:
         for colunm in row:
-            print("{}".format(colunm), end = " ")
+            print("{:d}".format(colunm), end = " ")
         print("")
