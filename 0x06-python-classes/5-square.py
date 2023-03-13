8674378eeab4e2a01915a4f62b4abff2ac1bd92a
Lorrine Adefemi <adefemilauren@gmail.com>
2023-03-13 09:57:05 -0700

    commiting task 5

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+""" create square class"""
+
+
+class Square:
+    """Represents a square
+    Attributes:
+        __size (int): size of a side of the square
+    """
+
+    def __init__(self, size=0):
+        """Initializes a square
+        Args:
+        size (int): size of a side of the square
+        Returns: None
+        """
+        self.__size = size
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        return self.__size ** 2
+
+    def my_print(self):
+        if self.__size <= 0:
+            print()
+        if self.size > 0:
+            for i in range(0, self.__size):
+                for j in range(0, self.__size):
+                   print("#", end = '')
+                print("")
