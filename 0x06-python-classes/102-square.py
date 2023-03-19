55267e06f8540bf1339aeed52e576a37104a2fe9
Lorrine Adefemi <adefemilauren@gmail.com>
2023-03-19 16:35:42 -0700

    commiting task 102

---
+++ 0x06-python-classes/102-square.py
@@ -0,0 +1,50 @@
+#!/usr/bin/python3
+""" create square class"""
+
+
+class Square:
+    """Represents a square
+    Attributes:
+        __size (int): size of a side of the square
+    """
+
+    def __init__(self, size=0):
+        """Initializes a square
+        Args:
+        size (int): size of a side of the square
+        Returns: None
+        """
+        self.__size = size
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        return self.__size ** 2
+
+    def __eq__(self, other):
+        return self.area() == other.area()
+
+    def __ne__(self, other):
+        return not self.__eq__(other)
+
+    def __gt__(self, other):
+        return self.area() > other.area()
+
+    def __ge__(self, other):
+        return self.area() >= other.area()
+
+    def __lt__(self, other):
+        return self.area() < other.area()
+
+    def __le__(self, other):
+        return self.area() <= other.area()
